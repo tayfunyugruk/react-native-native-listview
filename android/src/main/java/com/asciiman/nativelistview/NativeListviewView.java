9691b004333fdf48f870281ed72f8bf9d4f495cb
Tayfun YÜĞRÜK <tayfunyugruk@gmail.com>
2017-03-12 01:13:53 +0300

    native view test

--- android/src/main/java/com/asciiman/nativelistview/NativeListviewView.java
+++ android/src/main/java/com/asciiman/nativelistview/NativeListviewView.java
@@ -3,7 +3,6 @@ package com.asciiman.nativelistview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
